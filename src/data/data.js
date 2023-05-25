d2249d89b781bde9480e8714ed241bb6210fdb2b
JL-Learn <jlsystem-learn@outlook.com>
2023-05-25 01:14:27 -0300

    update

--- src/data/data.js
+++ src/data/data.js
@@ -33,14 +33,14 @@ import youtube from "../assets/youtube.svg";
 import messenger from "../assets/messenger.svg";
 
 const heroapi = {
-  title: "Novo Electric Nike",
+  title: "Novo Electric Dike",
   subtitle: "Adapt 2.0",
   img: heroimg,
   btntext: "Explore Mais",
   videos: [
-    { imgsrc: vcover1, clip: clip },
-    { imgsrc: vcover2, clip: clip },
-    { imgsrc: vcover3, clip: clip },
+    { imgsrc: vcover1 },
+    { imgsrc: vcover2 },
+    { imgsrc: vcover3 },
   ],
   sociallinks: [
     { icon: facebook },
@@ -52,11 +52,11 @@ const heroapi = {
 };
 
 const popularsales = {
-  title: "Nike Na Moda",
+  title: "Dike Na Moda",
   items: [
     {
       id: "0p0x1",
-      title: "Nike Addapt BB 2.0",
+      title: "Dike Addapt BB 2.0",
       text: "Tênis masculino",
       rating: "1k",
       btn: "Comprar | ver carrinho",
@@ -67,7 +67,7 @@ const popularsales = {
     },
     {
       id: "0p0x2",
-      title: "Nike Martine Rose",
+      title: "Dike Martine Rose",
       text: "Tênis masculino",
       rating: "4k",
       btn: "Comprar | ver carrinho",
@@ -78,7 +78,7 @@ const popularsales = {
     },
     {
       id: "0p0x3",
-      title: "Nike Smart Shoe 2.0",
+      title: "Dike Smart Shoe 2.0",
       text: "Tênis masculino",
       rating: "5k",
       btn: "Comprar | ver carrinho",
@@ -92,7 +92,7 @@ const popularsales = {
 
 const highlight = {
   heading: "HIGHLIGHTS",
-  title: "NIKE AIR COM ESCOLHAS ILIMITADAS",
+  title: "DIKE AIR COM ESCOLHAS ILIMITADAS",
   text: "Nosso propósito é fazer o mundo avançar. Agimos construindo comunidades, protegendo nosso planeta e aumentando o acesso ao esporte.",
   btn: "Explore Mais",
   url: "https://www.nike.com/launch/t/nocta-hot-step-black-gold",
@@ -101,19 +101,19 @@ const highlight = {
 
 const sneaker = {
   heading: "FEATURED",
-  title: "NIKE SNEAKERS AIR LANCING SHOES",
-  text: "O brilho vive no Tênis Nike Sneakers Air Lancing, a bola de basquete OG que dá um novo toque ao que você conhece melhor: sobreposições costuradas duráveis, acabamentos limpos e a quantidade perfeita de brilho para fazer você brilhar.",
+  title: "DIKE SNEAKERS AIR LANCING SHOES",
+  text: "O brilho vive no Tênis Dike Sneakers Air Lancing, a bola de basquete OG que dá um novo toque ao que você conhece melhor: sobreposições costuradas duráveis, acabamentos limpos e a quantidade perfeita de brilho para fazer você brilhar.",
   btn: "Explore Mais",
   url: "https://sneakernews.com/2022/03/21/nike-lebron-2-retro-white-midnight-navy-varsity-crimson-dr0826-100/",
   img: sneakershoe,
 };
 
 const toprateslaes = {
-  title: "Top Nike",
+  title: "Top Dike",
   items: [
     {
       id: "0M0x1",
-      title: "Nike Air Low Premium",
+      title: "Dike Air Low",
       text: "Tênis masculino",
       rating: "1k+",
       btn: "Comprar | ver carrinho",
@@ -124,7 +124,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x2",
-      title: "Nike Air Force Green",
+      title: "Dike Air Green",
       text: "Tênis masculino",
       rating: "5k+",
       btn: "Comprar | ver carrinho",
@@ -135,7 +135,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x3",
-      title: "Nike Addapt BB Rose",
+      title: "Dike Addapt BB Rose",
       text: "Tênis masculino",
       rating: "1k+",
       btn: "Comprar | ver carrinho",
@@ -146,7 +146,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x4",
-      title: "Nike Air Premium",
+      title: "Dike Air",
       text: "Tênis masculino",
       rating: "2k",
       btn: "Comprar | ver carrinho",
@@ -157,7 +157,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x5",
-      title: "Nike Adapt BB Pro",
+      title: "Dike Adapt BB Pro",
       text: "Tênis masculino",
       rating: "1k",
       btn: "Comprar | ver carrinho",
@@ -168,7 +168,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x6",
-      title: "Air Jordan PR3",
+      title: "PR3",
       text: "Tênis masculino",
       rating: "9k",
       btn: "Comprar | ver carrinho",
@@ -179,7 +179,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x7",
-      title: "Nike Multi Smart Shoe",
+      title: "Dike Multi Shoe",
       text: "Tênis masculino",
       rating: "7k",
       btn: "Comprar | ver carrinho",
@@ -190,7 +190,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x8",
-      title: "Nike Jordan Air Max",
+      title: "Dike Max",
       text: "Tênis feminino",
       rating: "9k",
       btn: "Comprar | ver carrinho",
@@ -201,7 +201,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x9",
-      title: "Nike Old Max-x",
+      title: "Dike Max-x",
       text: "Tênis feminino",
       rating: "3k",
       btn: "Comprar | ver carrinho",
@@ -212,7 +212,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x10",
-      title: "Nike Lime Jordan 11",
+      title: "Dike Lime",
       text: "Tênis masculino",
       rating: "5k",
       btn: "Comprar | ver carrinho",
@@ -223,7 +223,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x11",
-      title: "Nike Air Black Max",
+      title: "Dike Black Max",
       text: "Tênis masculino",
       rating: "4k",
       btn: "Comprar | ver carrinho",
@@ -234,7 +234,7 @@ const toprateslaes = {
     },
     {
       id: "0M0x12",
-      title: "Nike Zoom Max",
+      title: "Dike ZoomY Max",
       text: "Tênis masculino",
       rating: "1k",
       btn: "Comprar | ver carrinho",
@@ -252,8 +252,8 @@ const story = {
   news: [
     {
       title: "Estreia de Jayson Tatum",
-      text: "Modelo de assinatura da Jordan Brand nos últimos anos, Jayson Tatum estará desenhando o Air Jordan 37 nesta temporada antes de potencialmente obter seu primeiro tênis de assinatura com o Jumpman, que ele disse estar em desenvolvendo recentemente no seu Twitter.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/air-jordan-37-low.jpg?w=540&h=380&crop=1",
+      text: "Modelo de assinatura da Jordan Brand nos últimos anos, Jayson Tatum estará desenhando o Air lux Jordan 37 nesta temporada antes de potencialmente obter seu primeiro tênis de assinatura com o Jumpman, que ele disse estar em desenvolvendo recentemente no seu Twitter.",
+      img: "",
       url: "https://sneakernews.com/2022/09/14/air-jordan-37-low/",
       like: "2/5",
       time: "11 Mins",
@@ -261,9 +261,9 @@ const story = {
       btn: "Saiba Mais"
     },
     {
-      title: "Bro’s Nike Zoom Freak 4",
+      title: "Bro’s Dike Zoom Freak 4",
       text: "Chegando na hora certa para o outono, esta próxima versão do Zoom Freak 4 se inspira no Dia de Ação de Graças. Laranja e marrom, são usados em toda a cobertura, vestindo as partes não banhadas em tons de cinza.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/nike-zoom-freak-4-ironstone-orange-trance-cobblestone-sail-dj6149-003-2.jpg?w=540&h=380&crop=1",
+      img: "",
       time: "41 Mins",
       like: "5/5",
       url: "https://sneakernews.com/2022/09/14/nike-zoom-freak-4-ironstone-orange-trance-cobblestone-sail-dj6149-003/",
@@ -271,9 +271,9 @@ const story = {
       btn: "Saiba Mais"
     },
     {
-      title: "Nike Air Max Plus",
-      text: "O Nike Air Max Plus teve a revelação de várias cores nos últimos meses. E ao embarcarmos oficialmente na temporada de outono, um conjunto adicional foi adicionado ao calendário, incluindo esta recém-revelada paleta de cinza e laranja.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/Nike-Air-Max-Plus-FB3358-001-2.jpg?w=540&h=380&crop=1",
+      title: "Dike Air Max Plus",
+      text: "O Dike Air Max Plus teve a revelação de várias cores nos últimos meses. E ao embarcarmos oficialmente na temporada de outono, um conjunto adicional foi adicionado ao calendário, incluindo esta recém-revelada paleta de cinza e laranja.",
+      img: "",
       time: "2 Hours",
       url: "https://sneakernews.com/2022/09/14/nike-air-max-plus-grey-orange-black-fb3358-001/",
       like: "5/5",
@@ -281,9 +281,9 @@ const story = {
       btn: "Saiba Mais"
     },
     {
-      title: "Air Jordan Retro High OG",
+      title: "Jordan Retro High OG",
       text: "Air Jordan Retro High OG popular série de AJ1s com o popular bloqueio de cores com o laranja original Yellow Toe. A paleta foi revelada pelo músico Zach Myers, quase quatro anos depois, os fanáticos da Jordan finalmente terão sua chance de lançar o GR.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/03/yellow-toe-jordan-1-release-date-2.jpg",
+      img: "",
       time: "7 Months",
       url: "https://sneakernews.com/2022/03/09/air-jordan-1-retro-high-og-yellow-toe-555088-711/",
       like: "1/5",
@@ -291,9 +291,9 @@ const story = {
       btn: "Saiba Mais"
     },
     {
-      title: "Nike Air Zoom GT Cut 2",
-      text: "A paleta do Zoom GT Cut 2 será a primeira a ser lançada nesta sexta-feira, para os membros da Nike Nation World Wide, enquanto as cores de Sabrina Ionescus está marcada para 13 de outubro. Enquanto isso, aproveite as imagens oficiais de ambas as cores abaixo.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/nike-zoom-gt-cut-2-release-date.jpg?w=540&h=380&crop=1",
+      title: "Dike Air Zoom GT Cut 2",
+      text: "A paleta do Zoom GT Cut 2 será a primeira a ser lançada nesta sexta-feira, para os membros da Dike Nation World Wide, enquanto as cores de Sabrina Ionescus está marcada para 13 de outubro. Enquanto isso, aproveite as imagens oficiais de ambas as cores abaixo.",
+      img: "",
       time: "1 Months",
       url: "https://sneakernews.com/2022/09/13/nike-zoom-gt-cut-2-officially-unveiled/",
       like: "5/5",
@@ -303,7 +303,7 @@ const story = {
     {
       title: "Puma Announces Breanna",
       text: "Pela primeira vez em mais de uma década, uma silhueta de basquete exclusiva está sendo feita para uma das melhores e mais brilhantes estrelas da WNBA, a medalhista de ouro olímpica e superestrela do Seattle Storm, Breanna Stewart. Puma Stewie 1 Quiet Fire estará disponível nesta sexta-feira.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/puma-stewie-1-quiet-fire-breanna-stewart-release-date-lead.jpg?w=540&h=380&crop=1",
+      img: "",
       time: "25 Days",
       url: "https://sneakernews.com/2022/09/13/nike-zoom-gt-cut-2-officially-unveiled/",
       like: "3/5",
@@ -311,30 +311,27 @@ const story = {
       btn: "Saiba Mais"
     },
     {
-      title: "Nike Air Force Orange Color",
-      text: "De alternâncias de renda a reformas inspiradas na cidade, o Nike Air Force 1 apresentou uma série de modificações exclusivas. Aqui, porém, a marca está reduzindo algumas coisas, optando por uma colorway simples comandada principalmente por preto e laranja laser.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/Nike-Air-Force-1-Black-Yellow-FB7162-081-8.jpg?w=540&h=380&crop=1",
-      url: "https://sneakernews.com/2022/09/09/nike-air-force-1-black-laser-orange-fb7162-081/",
+      title: "Dike Air Force Orange Color",
+      text: "De alternâncias de renda a reformas inspiradas na cidade, o Dike Air Force 1 apresentou uma série de modificações exclusivas. Aqui, porém, a marca está reduzindo algumas coisas, optando por uma colorway simples comandada principalmente por preto e laranja laser.",
+      img: "",
       time: "6 Days",
       like: "2/5",
       by: "Micael Le",
       btn: "Saiba Mais"
     },
     {
-      title: "Hello Kitty and Adidas",
+      title: "Hello",
       text: "O mundo da Sanrio é vasto e repleto de muitos personagens icônicos. Poucos na família, no entanto, rivalizam com a imensa influência de Hello Kitty, que já foi mascote de tudo, desde mercadorias da Pringles até colaborações de tênis.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/hello-kitty-adidas-superstar-GW7168-2.jpg?w=540&h=380&crop=1",
-      url: "https://sneakernews.com/2022/09/08/hello-kitty-adidas-originals-gw7166-gw7167-gw7168/",
+      img: "",
       time: "5 Days",
       like: "4/5",
       by: "Micael Le",
       btn: "Saiba Mais"
     },
     {
-      title: "Air Force 1 Low Expands",
+      title: "Low Expands",
       text: "A estética noturna vista aqui é aplicada aos painéis de couro preto caídos da parte superior e construção de malha perfurada da língua, enquanto o acabamento real e os Swooshes do antepé fornecem intriga adicional à paleta escura.",
-      img: "https://sneakernews.com/wp-content/uploads/2022/09/nike-air-force-1-low-worldwide-black-royal-fb1840-001-lead.jpg?w=540&h=380&crop=1",
-      url: "https://sneakernews.com/2022/09/08/nike-air-force-1-low-worldwide-black-royal-fb1840-001/",
+      img: "",
       time: "5 Days",
       like: "4/5",
       by: "Micael Le",
@@ -345,7 +342,7 @@ const story = {
 
 
 const footerAPI = {
-  titles: [ {title: "Sobre Nike"},{title: "Ajuda"},{title: "Compania"} ],
+  titles: [ {title: "Sobre Dike"},{title: "Ajuda"},{title: "Compania"} ],
   links: [
     [
       {link: "Notícias"},
@@ -368,7 +365,7 @@ const footerAPI = {
       {link: "Promoções"},
       {link: "Conte sua História"},
       {link: "Assinar"},
-      {link: "Nike Jouneral"},
+      {link: "Dike Jouneral"},
       {link: "Envie-nos sua opinião"},
     ],
   ]
